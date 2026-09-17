df2fe9018d1655b7887e75d2e5926ca079e5f6d2
Your Name <your.email@example.com>
2026-09-17 14:25:01 -0700

    Implement comparison file and update the student file to return class list

--- activity/student.py
+++ activity/student.py
@@ -7,10 +7,11 @@ class Student:
 
     def add_class(self, class_name):
         self.classes.append(class_name)
+        return self.classes
 
     def get_num_classes(self):
         return len(self.classes)
 
     def summary(self):
-        return f"{self.name} is a {self.grade} enrolled in a {self.get_num_classes()} classes" 
+        return f"{self.name} is a {self.grade} enrolled in {self.get_num_classes()} classes" 
 
