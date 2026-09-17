35ad49eb4086bd7413a4882e00f995955476acc2
Your Name <your.email@example.com>
2026-09-17 11:36:08 -0700

    added class to student file

--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,16 @@
-# add your Student class here!
+class Student:
+
+    def __init__(self, name, grade, classes):
+        self.name = name
+        self.grade = grade
+        self.classes = classes
+
+    def add_class(self, class_name):
+        self.classes.append(class_name)
+
+    def get_num_classes(self):
+        return len(self.classes)
+
+    def summary(self):
+        return f"{self.name} is a {self.grade} enrolled in a {self.get_num_classes()} classes" 
+
